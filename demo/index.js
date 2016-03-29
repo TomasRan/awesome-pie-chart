b1b6c6a5285fa91d3400bc4454390c8cbdc0fbc3
tomasran <cs_ttr@163.com>
2016-03-29 15:36:00 +0800

    modify

--- demo/index.js
+++ demo/index.js
@@ -1,34 +1,70 @@
-var DoughGraph = require('../doughnut.js');
+var PieChart = require('../pie-chart.js');
 
-var doughGraph = new DoughGraph({
-	graph: {
-		outsideR: 100,
-		insideR: 60,
-		rotation: 23,
-		space: 1,
-		slices: [{
-			color: 'red',
-			percent: 0.3
-		}, {
-			color: 'green',
-			percent: 0.7
-		}]
-	},
-	description: {
-		'content': [{
-			'desc': '<span>dedw</span>',
-			'background': 'red'
-		}, {
-			'desc': '<span>dedw</span>',
-			'background': 'blue'
-		}],
-		'itemWidth': 50,
-		'borderWidth': 1,
-		'borderColor': '333',
-		'vspace': '10px',
-		'hspace': '10px'
+var graph = {
+}
+
+var pieChart = new PieChart({
+	'background': '#f7f7f7',
+	'relativePos': 'top',
+	'graph': {
+		'outsideR': 100,
+		'insideR': 60,
+		'rotation': -90,
+		'space': 1,
+		'title': {
+			'fontSize':	40,
+			'content': '<span>80<span>%</span></span>'
+		},
+		'slices': [{
+			'name': 'ab',
+			'color': '#f75106',
+			'percent': 0.3,
 			
+		}, {
+			'name': 'bc',
+			'color': '#13c819',
+			'percent': 0.7
+		}],
+		'callback': function(i) {alert(i);}
+	},
+	'description': {
+		'items': [{
+			'content': '<span><i class="sss"></i>对  123人</span>',
+			'background': '#fefefe',
+			'name': 'a'
+		}, {
+			'content': '<span>错  243人</span>',
+			'background': '#fefefe',
+			'name': 'b'
+		}, {
+			'content': '<span>对错 2423人</span>',
+			'name': 'c'
+		}, {
+			'content': '<span>嘚瑟 120人</span>',
+			'name': 'd'
+		}, {
+			'content': '<span>对错 2423人</span>',
+			'name': 'e'
+		}],
+		'callback': function(i) {alert(i);}
 	}
 });
 
-$('body').append(doughGraph.getNode());
+$('.main').append(pieChart.getNode());
+
+pieChart.fresh({
+	'slices': [{
+		'name': 'ab',
+		'percent': 0.5
+	}, {
+		'name': 'bc',
+		'percent': 0.5
+	}],
+	'items': [{
+		'name': 'a',
+		'content': '121'
+	}, {
+		'name': 'b',
+		'content': 'daewdaw'
+	}]
+});
